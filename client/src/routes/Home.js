93a1fc89875676421a420dc852236e035fce6c91
Vedanth Bora <vedanthbora@gmail.com>
2022-04-01 02:03:03 +0530

    [FEAT]: added homepage videoBanner component

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Banner from "../components/home-page/Banner";
+import FeaturedBrands from "../components/home-page/FeaturedBrands";
+import FeaturedCategories from "../components/home-page/FeaturedCategories";
+import VideoBanner from "../components/home-page/VideoBanner";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="relative" style={{ top: "5rem" }}>
+      <Banner />
+
+      <div class="spacer-3rem"></div>
+      <div class="spacer-3rem"></div>
+      <div class="spacer-3rem"></div>
+
+      <main>
+        <FeaturedCategories />
+
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+
+        <FeaturedBrands />
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+
+        <VideoBanner />
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+        <div class="spacer-3rem"></div>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
